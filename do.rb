af1477ce3e635ce8e97ac23deea1a84def5b1271
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2017-02-05 02:21:12 -0500

    Big square figured out

--- do.rb
+++ do.rb
@@ -6,6 +6,7 @@ png[1,1] = ChunkyPNG::Color.rgba(10, 20, 30, 128)
 # png.save('filename.png', :interlace => true)
 
 pic = ChunkyPNG::Image.from_file('./test.png')
+pic2 = ChunkyPNG::Image.from_file('./test.png')
 Indico.api_key =  '48dbdf05055a75d5872b71d412890394'
 
 # single example
@@ -18,4 +19,11 @@ print bottom_right_corner
 pic.rect(top_left_corner[0], top_left_corner[1],
                        bottom_right_corner[0], bottom_right_corner[1],
                        :red )
-pic.save('newtest.png')
+pic.save('square.png')
+
+pic2.rect(top_left_corner[0] - 20,
+          top_left_corner[1] - 20,
+          bottom_right_corner[0] + 20,
+          bottom_right_corner[1] + 20,
+          :red )
+pic2.save('big_square.png')
